b451f3c99466b00534c1f852d5696c4a9794faa2
Ardon Kotey <koteyardon2002@gmail.com>
2023-02-15 18:04:06 +0530

    mercedes

--- first.py
+++ first.py
@@ -3,3 +3,4 @@ print("This is the first program")
 a =50
 b =300
 print("The sum of two numbers is",a+b)
+print('hello world')
--- second.py
+++ second.py
@@ -3,4 +3,5 @@ print("This is the second program")
 a=100
 b=20
 c=2
+d-40
 print("The difference is ", a-b)
